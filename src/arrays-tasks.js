8ad78b08fb160b497beed8da760b3dc447116ee6
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-01-13 19:19:44 +0300

    feat: implement findLongestIncreasingSubsequence fn

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -514,8 +514,20 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let maxLen = 0;
+  let count = 0;
+
+  nums.reduce((prev, cur) => {
+    count += 1;
+    if (prev > cur) {
+      maxLen = maxLen > count ? maxLen : count;
+      count = 1;
+    }
+    return cur;
+  });
+
+  return maxLen;
 }
 
 /**
