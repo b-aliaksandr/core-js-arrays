386cf5f59ff8d2046f2ed7743878e871e0cce966
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-01-13 19:18:43 +0300

    feat: implement getIdentityMatrix fn

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -422,8 +422,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (outer, outerIndex) =>
+    Array.from({ length: n }, (inner, innerIndex) =>
+      outerIndex === innerIndex ? 1 : 0
+    )
+  );
 }
 
 /**
