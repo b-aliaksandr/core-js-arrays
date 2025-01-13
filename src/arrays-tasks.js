3f0ccd6e198df9656894527eeda53307e3acd08e
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-01-13 19:18:02 +0300

    feat: implement generateOdds fn

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -365,8 +365,11 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len * 2 }, () => 0).reduce((acc, cur, index) => {
+    if (index % 2 !== 0) acc.push(index);
+    return acc;
+  }, []);
 }
 
 /**
