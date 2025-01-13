8fbdc182c230aca68e91c36994ee1ddd2982f86e
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-01-13 09:47:10 +0300

    feat: implement sumArrays fn

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -40,8 +40,13 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const [biggestArr, smallerArr] =
+    arr1.length > arr2.length ? [arr1, arr2] : [arr2, arr1];
+  return biggestArr.map((item, index) => {
+    if (smallerArr[index] === undefined) return item;
+    return item + smallerArr.at(index);
+  });
 }
 
 /**
